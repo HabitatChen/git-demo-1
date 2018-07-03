1f825bb0de60d841f935347ba0bc996c98aa6a15
habitatchen <habitatchen@163.com>
2018-07-03 23:23:25 +0800

    js修改

--- project/canvas/main.js
+++ project/canvas/main.js
@@ -1,5 +1,5 @@
-var yyy = document.getElementById('xxx')
-var context = yyy.getContext('2d')
+var yyy = document.getElementById('xxx');
+var context = yyy.getContext('2d');
 
 autoSetCanvasSize(yyy);
 
