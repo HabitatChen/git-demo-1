cc659c7f571ddc98b17217828e4ae20d8a2baf0a
habitatchen <habitatchen@163.com>
2018-07-11 23:13:15 +0800

    轮播优化

--- project/无缝轮播/main.js
+++ project/无缝轮播/main.js
@@ -1,40 +1,72 @@
-let n 
-初始化()
-setInterval(() => {
-  makeLeave(getImage(n))
-      .one('transitionend', (e) => {
-      makeEnter($(e.currentTarget))
-    })
-  makeCurrent(getImage(n+1))
-  n += 1
-}, 1500)
+let $buttons = $('#buttonWrapper > button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0;
 
 
-function getImage(n){
-  return $(`.images > img:nth-child(${x(n)})`)
+makeFakeSlides()
+$slides.css({ transform: 'translateX(-400px)' })
+bindEvents()
+$(next).on('click',()=>{
+    goTOSlide(current+1)
+})
+$(previous).on('click',()=>{
+    goTOSlide(current-1)
+})
+
+let timer = setInterval(function(){
+  goTOSlide(current + 1)
+},2000)
+
+$('.window').on('mouseenter',function(){
+  window.clearInterval(timer)
+}).on('mouseleave',function(){
+  timer = setInterval(function(){
+    goTOSlide(current + 1)
+  },2000)
+})
+
+
+function bindEvents(){
+  $('#buttonWrapper').on('click','button',e=>{
+    let $button = $(e.currentTarget)
+    let index = $button.index()
+    goTOSlide(index)
+  })
 }
 
-function x(n) {
-  if (n > 3) {
-    n = n % 3
-    if (n === 0) {
-      n = 3
-    }
-  } // n = 1 2 3
-  return n
-}
-function 初始化() {
-  n = 1;
-  $(`.images > img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
+
+
+function goTOSlide(index) {
+  if (index > $buttons.length - 1) {
+    index =0
+  } else if (index < 0) {
+    index = $buttons.length - 1;
+  }
+  if(current === $buttons.length - 1 && index ===0) {
+    //最后一张到第一张
+    $slides.css({transform:`translateX(${-($buttons.length+1)*400}px)`})
+      .one('transitionend',function(){
+        $slides.hide() .offset()
+        $slides.css({transform:`translateX(${-(index +1 )*400}px)`}) .show()
+      })
+  } else if (current ===0 && index ===$buttons.length -1) {
+      $slides.css({ transform: `translateX(0px)`})
+        .one('transitionend',()=>{
+          $slides.hide().offset()
+          $slides.css({transform:`translateX(${-(index +1 )*400}px)`}).show()
+        })
+  } else{
+    $slides.css({transform:`translateX(${-(index +1 )*400}px)`})
+  }
+    current = index
 }
 
-function makeCurrent($node){
-  return  $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node){
-  return  $node.removeClass('enter current').addClass('leave')
-}
-function makeEnter($node){
-  return  $node.removeClass('enter leave').addClass('enter')
+
+function makeFakeSlides() {
+  let $firstCopy = $images.eq(0).clone(true)
+  let $lastCopy = $images.eq($images.length - 1).clone(true)
+  //加 true 是复制他全家
+  $slides.append($firstCopy) //把拷贝的第一张加到最前面
+  $slides.prepend($lastCopy) //把拷贝的最后一张放到最后面
 }
