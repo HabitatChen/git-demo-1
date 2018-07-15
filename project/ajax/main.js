a9a6ccdd5ecf2078ee0990d0dca4611a6d8ba2cb
habitatchen <habitatchen@163.com>
2018-07-15 12:43:47 +0800

    封装jQuery

--- project/ajax/main.js
+++ project/ajax/main.js
@@ -4,54 +4,41 @@ window.jQuery = function (nodeOrSelector) {
 	nodes.html = function () { };
 	return nodes
 }
-window.jQuery.ajax = function ({url,method,body,successFn,failFn,headers}) {
-	// let url = options.url
-	// let method = options.method
-	// let body = options.body
-	// let successFn = options.successFn
-	// let failFn = options.failFn
-	// let headers = options.headers
-	//ES6 析构赋值
-	//let {url,method,body,successFn,failFn,headers} = options
-	//将解构直接放到参数里头
 
-	let request = new XMLHttpRequest()
-	request.open(method, url)//配置request
-
-	for (let key in headers) {
-		let value = headers[key]
-		request.setRequestHeader(key,value)
-	}
-	request.onreadystatechange = () => {
-		if (request.readyState === 4) {
-			if (request.status >= 200 && request.status <= 300) {
-				successFn.call(undefined, request.responseText)
-			} else if (request.status >= 400) {
-				failFn.call(undefined, request)
-			}
-		}
-	}
-	request.send(body)
-}
 window.$ = window.jQuery
 
-function f1(responseText){}
-function f2(responseText){}
+window.jQuery.ajax = function ({ url, method, body, headers }) {
+	return new Promise(function (resolve,reject) {
+		let request = new XMLHttpRequest()
+		request.open(method, url)//配置request
+		for (let key in headers) {
+			let value = headers[key]
+			request.setRequestHeader(key, value)
+		}
+		request.onreadystatechange = () => {
+			if (request.readyState === 4) {
+				if (request.status >= 200 && request.status <= 300) {
+					resolve.call(undefined, request.responseText)
+				} else if (request.status >= 400) {
+					reject.call(undefined, request)
+				}
+			}
+		}
+		request.send(body)
+	})
+}
 
 myButton.addEventListener('click', (e) => {
 	window.$.ajax({
 		url: '/xxx',
 		method: 'get',
-		headers:{
-			'content-type':'application/x-www-form-urlencoded',
-			'ck':17,
-		},
-		successFn: (x) => {
-			f1.call(undefined,x)
-			f2.call(undefined,x)
-		},   
-		failFn: () => { },
-	})
+		headers: {
+			'content-type': 'application/x-www-form-urlencoded',
+			'ck': 17,
+		}
+	}).then((text)=>{console.log(text)},
+			(request)=>{console.log(request)}
+)
 })
 
 
